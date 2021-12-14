d325f126a6f102588f57f436c84cd982491f0ad4
Sadik Ozer <sadikozr@gmail.com>
2021-12-14 21:51:16 +0300

    Add day1 & day2 answers
    Signed-off-by: Sadik Ozer <sadikozr@gmail.com>

--- day01/day1.py
+++ day01/day1.py
@@ -1,4 +1,43 @@
-
 import sys
+from day1_1_input import *
+from day1_2_input import *
+
+numbers=list()
+for line in s.split("\n"):
+	if line == "":
+		continue
+	numbers.append(int(line))
 
 
+def process_question1():
+	counter = 0
+	k = 0
+	for i in numbers[1:]:
+	    if i > numbers[k]:
+	        counter += 1
+	    k += 1
+
+	print("Q1 Answer: ", counter)
+
+
+def process_question2():
+	l = list()
+	for i in range(0, len(numbers)-2):
+	    a = numbers[i] + numbers[i+1] + numbers[i+2]
+	    #print(a)
+	    l.append(a)
+	    
+
+	counter = 0
+	k = 0
+	for i in l[1:]:
+	    if i > l[k]:
+	        counter += 1
+	    k += 1
+	
+	print("Q2 Answer: ", counter)
+
+
+
+process_question1()
+process_question2()
--- day02/day2.py
+++ day02/day2.py
@@ -1,22 +1,56 @@
 import sys
-#from day2_1_input import *
+from day2_1_input import *
 from day2_2_input import *
 
-h = 0
-p = 0
+cmd = list()
 
-aim = 0
-for s, pos in l:
-    if s == "forward":
-        h += pos
-        p += (aim * pos)
-    if s == "down":
-        #p += pos
-        aim += pos
-    if s == "up":
-        #p -= pos
-        aim -= pos
+for line in s.split("\n"):
+	if line == "":
+		continue
+	d, pos = line.strip().split()
 
-print("h", h)
-print("p", p)
-print("h*p", h*p)
+	cmd.append( (d, int(pos)))
+
+
+def process_question1():
+	h = 0
+	p = 0
+
+	for s, pos in cmd:
+	    if s == "forward":
+	        h += pos
+	    if s == "down":
+	        p += pos
+	    if s == "up":
+	        p -= pos
+
+	print("h", h)
+	print("p", p)
+	print("Q1 Answer (h*p): ", h*p)
+
+
+def process_question2():
+	h = 0
+	p = 0
+
+	aim = 0
+	for s, pos in cmd:
+	    if s == "forward":
+	        h += pos
+	        p += (aim * pos)
+	    if s == "down":
+	        #p += pos
+	        aim += pos
+	    if s == "up":
+	        #p -= pos
+	        aim -= pos
+
+	print("h", h)
+	print("p", p)
+	
+	print("Q2 Answer (h*p): ", h*p)
+
+
+
+process_question1()
+process_question2()
